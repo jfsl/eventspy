e66f4c72e5e8f3735f5ebb9db6976f7ab993d7d3
jfsl <johanfsl@gmail.com>
2013-03-05 11:12:00 +0100

    convert indentation to spaces

--- eventspy.js
+++ eventspy.js
@@ -1,49 +1,49 @@
 var port = chrome.extension.connect({
-	name : "eventspy"
+  name : "eventspy"
 });
 
 port.onDisconnect.addListener(function (evt) {
   port.postMessage({'action': 'unsubscribe'});
-	port = undefined;
+  port = undefined;
 });
 
 chrome.devtools.panels.create("Event Spy", "devtools_icon.png", "devtools_tab.html", function(panel) {
-	var _window;
+  var _window;
 
-	function drawEventDiv(evtObj) {
-		var eventDiv      = _window.document.createElement('h3'),
-		    codeDiv       = _window.document.createElement('div'),
-		    eventDate;
+  function drawEventDiv(evtObj) {
+    var eventDiv      = _window.document.createElement('h3'),
+        codeDiv       = _window.document.createElement('div'),
+        eventDate;
 
     if (evtObj.eventspyType === 'fired') {
     
       eventDate = new Date(evtObj.data.event.timeStamp);
     
-		  eventDiv.innerHTML = [eventDate.toLocaleTimeString(), " -> ",
-						                evtObj.data.event.name, " : ",
-						                evtObj.data.event.type, " (",				
-						                evtObj.data.event.pageX, ", ",
-						                evtObj.data.event.pageY, ")"].join('');
-						     
-	    codeDiv.innerHTML = evtObj.data.event.handler;
+      eventDiv.innerHTML = [eventDate.toLocaleTimeString(), " -> ",
+                            evtObj.data.event.name, " : ",
+                            evtObj.data.event.type, " (",       
+                            evtObj.data.event.pageX, ", ",
+                            evtObj.data.event.pageY, ")"].join('');
+                 
+      codeDiv.innerHTML = evtObj.data.event.handler;
     
     } else if (evtObj.eventspyType === 'created') {
-      eventDiv.innerHTML = [evtObj.data.type, " : useCapture(",				
-						                evtObj.data.useCapture, ") "].join('');
-						                
-	    codeDiv.innerHTML = evtObj.data.listener;
-			
+      eventDiv.innerHTML = [evtObj.data.type, " : useCapture(",       
+                            evtObj.data.useCapture, ") "].join('');
+                            
+      codeDiv.innerHTML = evtObj.data.listener;
+      
     }
     
-		eventDiv.addEventListener('mouseover', function (evt) {
-			
-			port.postMessage({
-			  'action': 'highlight', 
-			  'targetNodeID': evtObj.data.event.targetNodeID,
-  		  'tabId': chrome.devtools.inspectedWindow.tabId 
-			});
-			
-		}, false);
+    eventDiv.addEventListener('mouseover', function (evt) {
+      
+      port.postMessage({
+        'action': 'highlight', 
+        'targetNodeID': evtObj.data.event.targetNodeID,
+        'tabId': chrome.devtools.inspectedWindow.tabId 
+      });
+      
+    }, false);
 
     $(eventDiv).addClass( "ui-accordion-header ui-helper-reset ui-state-default ui-corner-all" )
                .hover(function () {
@@ -71,85 +71,85 @@ chrome.devtools.panels.create("Event Spy", "devtools_icon.png", "devtools_tab.ht
     
     $(eventDiv)
     
-		return {'eventdiv': eventDiv, 'codediv': codeDiv}
-	}
+    return {'eventdiv': eventDiv, 'codediv': codeDiv}
+  }
 
-	function drawBulk(dump) {
-		var idx = 0,
-		    logFragment = document.createDocumentFragment(),
-		    registeredFragment = document.createDocumentFragment(),
-	      logTab = _window.$('#log-tab'),
-	      registeredTab = _window.$('#registered-tab');
+  function drawBulk(dump) {
+    var idx = 0,
+        logFragment = document.createDocumentFragment(),
+        registeredFragment = document.createDocumentFragment(),
+        logTab = _window.$('#log-tab'),
+        registeredTab = _window.$('#registered-tab');
 
-		dump.forEach(function(eventObj) {
-		  var divs;
-		  
-		  if (eventObj && eventObj.eventspyType) {
-  		  divs = drawEventDiv(eventObj);
-		  } 
+    dump.forEach(function(eventObj) {
+      var divs;
+      
+      if (eventObj && eventObj.eventspyType) {
+        divs = drawEventDiv(eventObj);
+      } 
 
-		  if (eventObj && eventObj.eventspyType === 'fired') {
+      if (eventObj && eventObj.eventspyType === 'fired') {
         logFragment.appendChild(divs.eventdiv);
-  			logFragment.appendChild(divs.codediv);	
-		  } else if (eventObj && eventObj.eventspyType === 'created') {
-		    registeredFragment.appendChild(divs.eventdiv);
-  			registeredFragment.appendChild(divs.codediv);
-		  }
-			
-		});
+        logFragment.appendChild(divs.codediv);  
+      } else if (eventObj && eventObj.eventspyType === 'created') {
+        registeredFragment.appendChild(divs.eventdiv);
+        registeredFragment.appendChild(divs.codediv);
+      }
+      
+    });
   
     logTab.append(logFragment);
     registeredTab.append(registeredFragment);
     
     _window.document.getElementById('log-tab').scrollTop = _window.document.getElementById('log-tab').scrollHeight;
     _window.document.getElementById('registered-tab').scrollTop = _window.document.getElementById('registered-tab').scrollHeight;
-	}
+  }
 
-	panel.onShown.addListener(function(pWindow) {
+  panel.onShown.addListener(function(pWindow) {
     port.postMessage({
-		  'action' : "subscribe",
-		  'tabId': chrome.devtools.inspectedWindow.tabId 
-	  });
-	  
-	  port.postMessage({
-	    'action': 'start-frontend',
-		  'tabId': chrome.devtools.inspectedWindow.tabId 
-	  });
-	  
-		_window       = pWindow,
-		
-		_window.$('#tabs').css({
-		  'height': (_window.$(_window).height() - 10)
-		});
-		
-		_window.$('#tabs').tabs({heightStyle: 'fill'});
-		
-		_window.$(_window).resize(function () {
-		
-			_window.$('#tabs').css({
-		    'height': (_window.$(_window).height() - 10)
-  		});
-		
-		  _window.$('#tabs').tabs('refresh');
-		});
-		
-	  port.onMessage.addListener(function(msg) {
-		  switch (msg.action) {
-	      case 'event-dump': {
-		      if (msg.dump != undefined && msg.dump.length > 0) {
-				      drawBulk(msg.dump); 
-		      }
-	        break;
-	      }
-	      
-	      case 'tab-updated': {
-	        port.postMessage({
-      	    'action': 'start-frontend',
-		        'tabId': chrome.devtools.inspectedWindow.tabId 
-	        });
-	        break;
-	      }
-		  }
-	  });
-	});
+      'action' : "subscribe",
+      'tabId': chrome.devtools.inspectedWindow.tabId 
+    });
+    
+    port.postMessage({
+      'action': 'start-frontend',
+      'tabId': chrome.devtools.inspectedWindow.tabId 
+    });
+    
+    _window       = pWindow,
+    
+    _window.$('#tabs').css({
+      'height': (_window.$(_window).height() - 10)
+    });
+    
+    _window.$('#tabs').tabs({heightStyle: 'fill'});
+    
+    _window.$(_window).resize(function () {
+    
+      _window.$('#tabs').css({
+        'height': (_window.$(_window).height() - 10)
+      });
+    
+      _window.$('#tabs').tabs('refresh');
+    });
+    
+    port.onMessage.addListener(function(msg) {
+      switch (msg.action) {
+        case 'event-dump': {
+          if (msg.dump != undefined && msg.dump.length > 0) {
+              drawBulk(msg.dump); 
+          }
+          break;
+        }
+        
+        case 'tab-updated': {
+          port.postMessage({
+            'action': 'start-frontend',
+            'tabId': chrome.devtools.inspectedWindow.tabId 
+          });
+          break;
+        }
+      }
+    });
+  });
 });
